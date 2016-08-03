70dff56c6f022c57d7f5d7c6fdbe80e161b3ff38
00Mugen00 <genericblue36@gmail.com>
2016-08-03 23:14:45 +0200

    Add files via upload

---
+++ ProgressBar.js
@@ -0,0 +1,39 @@
+var myVideo,play,bar,progress,max;
+max=400;
+function initialize(){
+	myVideo=document.getElementById("myVideo");
+	play=document.getElementById("play");
+	bar=document.getElementById("bar");
+	progress=document.getElementById("progress");
+	play.addEventListener("click",clicking,false);
+	bar.addEventListener("click",forwarding,false);
+	
+}
+
+function clicking(){
+	if(myVideo.paused==false){
+		myVideo.pause();
+		play.innerHTML="Play";
+	}else{
+		play.innerHTML="Pause";
+		myVideo.play();
+		bucle=setInterval(state,20);
+	}
+}
+
+function state(){
+	var total=parseInt(myVideo.currentTime*max/myVideo.duration);
+	progress.style.width=total+"px";
+}
+
+function forwarding(posicion){
+	if((myVideo.paused==false)&&(myVideo.ended==false)){
+		var ratonX=posicion.pageX-bar.offsetLeft;
+		var nuevoTiempo=(ratonX*myVideo.duration)/max;
+		myVideo.currentTime=nuevoTiempo;
+		progress.style.width=ratonX+"px";
+	}
+}
+
+
+window.addEventListener("load",initialize,false);
